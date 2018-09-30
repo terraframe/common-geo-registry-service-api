bbdba42320ea845cbe35bcc140d7a948b23a7ddf
jmapping <j.mapping@gmail.com>
2018-09-29 18:22:48 -0600

    Maven setup

---
+++ cgradapter-java/src/main/java/com/cgr/adapter/common/metadata/AttributeNumericType.java
@@ -0,0 +1,9 @@
+package com.cgr.adapter.common.metadata;
+
+public abstract class AttributeNumericType extends AttributePrimitiveType
+{
+  public AttributeNumericType(String _name, String _localizedLabel, String _localizedDescription, String _type)
+  {
+    super(_name, _localizedLabel, _localizedDescription, _type);
+  }
+}
