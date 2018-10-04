e1c42aa23e4ccc62492bfdaf6500bc93f606b5f5
rrowlands <rrowlands@terraframe.com>
2018-10-04 17:11:00 -0600

    TestFixture and MetadataFactory.

--- cgradapter-java/src/test/java/org/commongeoregistry/adapter/SerializationTest.java
+++ cgradapter-java/src/test/java/org/commongeoregistry/adapter/SerializationTest.java
@@ -13,39 +13,19 @@ import org.commongeoregistry.adapter.metadata.AttributeTermType;
 import org.commongeoregistry.adapter.metadata.AttributeType;
 import org.commongeoregistry.adapter.metadata.GeoObjectType;
 import org.commongeoregistry.adapter.metadata.HierarchyType;
+import org.commongeoregistry.adapter.metadata.MetadataFactory;
 import org.commongeoregistry.adapter.metadata.HierarchyType.HierarchyNode;
 import org.junit.Test;
 import org.locationtech.jts.util.Assert;
 
 public class SerializationTest
 {
-  private static String                  PROVINCE                   = "PROVINCE";
-  
-  private static String                  DISTRICT                   = "DISTRICT";
-  
-  private static String                  COMMUNE                    = "COMMUNE";
-  
-  private static String                  VILLAGE                    = "VILLAGE";
-  
-  private static String                  HOUSEHOLD                  = "HOUSEHOLD";
-  
-  private static String                  FOCUS_AREA                 = "FOCUS_AREA";
-  
-  private static String                  HEALTH_FACILITY            = "HEALTH_FACILITY";
-  
-  private static String                  HEALTH_FACILITY_ATTRIBUTE  = "healthFacilityType";
-
-  private static String                  GEOPOLITICAL               = "GEOPOLITICAL";
-
-  private static String                  HEALTH_ADMINISTRATIVE      = "HEALTH_ADMINISTRATIVE";
-  
   @Test
   public void testGeoObject()
   {
     RegistryServerInterface registry = new RegistryServerInterface();
     
-    GeoObjectType province = new GeoObjectType("State", GeometryType.POLYGON, "State", "", registry);
-    registry.getMetadataCache().addGeoObjectType(province);
+    MetadataFactory.newGeoObjectType("State", GeometryType.POLYGON, "State", "", registry);
     
     String geom = "POLYGON ((10000 10000, 12300 40000, 16800 50000, 12354 60000, 13354 60000, 17800 50000, 13300 40000, 11000 10000, 10000 10000))";
     
@@ -72,8 +52,7 @@ public class SerializationTest
   {
     RegistryServerInterface registry = new RegistryServerInterface();
     
-    GeoObjectType province = new GeoObjectType("State", GeometryType.POLYGON, "State", "", registry);
-    registry.getMetadataCache().addGeoObjectType(province);
+    MetadataFactory.newGeoObjectType("State", GeometryType.POLYGON, "State", "", registry);
     
     GeoObject geoObject = registry.createGeoObject("State");
     
@@ -89,8 +68,7 @@ public class SerializationTest
   {
     RegistryServerInterface registry = new RegistryServerInterface();
     
-    GeoObjectType state = new GeoObjectType("State", GeometryType.POLYGON, "State", "", registry);
-    registry.getMetadataCache().addGeoObjectType(state);
+    GeoObjectType state = MetadataFactory.newGeoObjectType("State", GeometryType.POLYGON, "State", "", registry);
     
     String sJson = state.toJSON().toString();
     GeoObjectType state2 = GeoObjectType.fromJSON(sJson, registry);
@@ -105,8 +83,7 @@ public class SerializationTest
   {
     RegistryServerInterface registryServerInterface = new RegistryServerInterface();
     
-    GeoObjectType state = new GeoObjectType("State", GeometryType.POLYGON, "State", "", registryServerInterface);
-    registryServerInterface.getMetadataCache().addGeoObjectType(state);
+    GeoObjectType state = MetadataFactory.newGeoObjectType("State", GeometryType.POLYGON, "State", "", registryServerInterface);
     
     AttributeType testChar = AttributeType.factory("testChar",  "testCharLocalName", "testCharLocalDescrip", AttributeCharacterType.TYPE);
     AttributeType testDate = AttributeType.factory("testDate",  "testDateLocalName", "testDateLocalDescrip", AttributeDateType.TYPE);
@@ -153,8 +130,7 @@ public class SerializationTest
   {
     RegistryServerInterface registry = new RegistryServerInterface();
     
-    GeoObjectType state = new GeoObjectType("State", GeometryType.POLYGON, "State", "", registry);
-    registry.getMetadataCache().addGeoObjectType(state);
+    GeoObjectType state = MetadataFactory.newGeoObjectType("State", GeometryType.POLYGON, "State", "", registry);
     
     AttributeType testChar = AttributeType.factory("testChar", "testCharLocalName", "testCharLocalDescrip", AttributeCharacterType.TYPE);
     AttributeType testDate = AttributeType.factory("testDate", "testDateLocalName", "testDateLocalDescrip", AttributeDateType.TYPE);
@@ -182,43 +158,9 @@ public class SerializationTest
   {
     RegistryServerInterface registry = new RegistryServerInterface();
     
-    // Define GeoObject Types
-    GeoObjectType province = new GeoObjectType(PROVINCE, GeometryType.POLYGON, "Province", "", registry);
-    registry.getMetadataCache().addGeoObjectType(province);
+    TestFixture.defineExampleHierarchies(registry);
     
-    GeoObjectType district = new GeoObjectType(DISTRICT, GeometryType.POLYGON, "District", "", registry);
-    registry.getMetadataCache().addGeoObjectType(district);
-    
-    GeoObjectType commune = new GeoObjectType(COMMUNE, GeometryType.POLYGON, "Commune", "", registry);
-    registry.getMetadataCache().addGeoObjectType(commune);
-    
-    GeoObjectType village = new GeoObjectType(VILLAGE, GeometryType.POLYGON, "Village", "", registry);
-    registry.getMetadataCache().addGeoObjectType(village);
-    
-    GeoObjectType household = new GeoObjectType(HOUSEHOLD, GeometryType.POLYGON, "Household", "", registry);
-    registry.getMetadataCache().addGeoObjectType(household);
-    
-    GeoObjectType focusArea = new GeoObjectType(FOCUS_AREA, GeometryType.POLYGON, "Focus Area", "", registry);
-    registry.getMetadataCache().addGeoObjectType(focusArea);
-    
-    GeoObjectType healthFacility = new GeoObjectType(HEALTH_FACILITY, GeometryType.POLYGON, "Health Facility", "", registry);    
-    registry.getMetadataCache().addGeoObjectType(healthFacility);
-    
-    // Define Geopolitical Hierarchy Type
-    HierarchyType geoPolitical = new HierarchyType(GEOPOLITICAL, "Geopolitical", "Geopolitical Hierarchy");   
-    HierarchyNode geoProvinceNode = new HierarchyType.HierarchyNode(province);
-    HierarchyNode geoDistrictNode = new HierarchyType.HierarchyNode(district);
-    HierarchyNode geoCommuneNode = new HierarchyType.HierarchyNode(commune);
-    HierarchyNode geoVillageNode = new HierarchyType.HierarchyNode(village);
-    HierarchyNode geoHouseholdNode = new HierarchyType.HierarchyNode(household);
-    
-    geoProvinceNode.addChild(geoDistrictNode);
-    geoDistrictNode.addChild(geoCommuneNode);
-    geoCommuneNode.addChild(geoVillageNode);
-    geoVillageNode.addChild(geoHouseholdNode);
-    
-    geoPolitical.addRootGeoObjects(geoProvinceNode);
-    
+    HierarchyType geoPolitical = registry.getMetadataCache().getHierachyType(TestFixture.GEOPOLITICAL).get();
     
     String geoPoliticalJson = geoPolitical.toJSON().toString();
     System.out.println(geoPoliticalJson);
