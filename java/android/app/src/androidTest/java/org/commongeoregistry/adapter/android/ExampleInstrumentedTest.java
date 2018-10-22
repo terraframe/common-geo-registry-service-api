31fcbdd7acdf41c73d0185bebdc9d1c1ab5e2313
rrowlands <rrowlands@terraframe.com>
2018-10-22 11:20:35 -0600

    Android project foundation.

---
+++ java/android/app/src/androidTest/java/org/commongeoregistry/adapter/android/ExampleInstrumentedTest.java
@@ -0,0 +1,30 @@
+package org.commongeoregistry.adapter.android;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        File filesDir = appContext.getFilesDir();
+
+        assertEquals("org.commongeoregistry.adapter.android", appContext.getPackageName());
+    }
+}
---
+++ java/android/app/src/main/java/org/commongeoregistry/adapter/android/MainActivity.java
@@ -0,0 +1,14 @@
+package org.commongeoregistry.adapter.android;
+
+import android.adapter.commongeoregistry.org.cgradapter_android.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
