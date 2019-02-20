0db1a57c0b0588be64ac9f75f09542ea563b5910
rrowlands <rrowlands@terraframe.com>
2019-02-20 15:51:51 -0700

    Fixed issue with attribute dates being represented as longs.

--- java/common/src/main/java/org/commongeoregistry/adapter/dataaccess/AttributeDate.java
+++ java/common/src/main/java/org/commongeoregistry/adapter/dataaccess/AttributeDate.java
@@ -1,28 +1,8 @@
-/**
- * Copyright (c) 2019 TerraFrame, Inc. All rights reserved.
- *
- * This file is part of Common Geo Registry Adapter(tm).
- *
- * Common Geo Registry Adapter(tm) is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * Common Geo Registry Adapter(tm) is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Common Geo Registry Adapter(tm).  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.commongeoregistry.adapter.dataaccess;
 
-import java.text.ParseException;
 import java.util.Date;
 
 import org.commongeoregistry.adapter.RegistryAdapter;
-import org.commongeoregistry.adapter.constants.AdapterConstants;
 import org.commongeoregistry.adapter.metadata.AttributeDateType;
 
 import com.google.gson.JsonElement;
@@ -35,54 +15,46 @@ public class AttributeDate extends Attribute
    * 
    */
   private static final long serialVersionUID = 5532076653984789765L;
-  
-  private Long date;
-  
+
+  private Date              date;
+
   public AttributeDate(String name)
   {
     super(name, AttributeDateType.TYPE);
-    
+
     this.date = null;
   }
-  
+
   @Override
   public void setValue(Object date)
   {
-    if(date instanceof Long)
-    {
-      this.setDate((Long)date);
-    }
+    this.setDate((Date) date);
   }
-  
 
-  public void setDate(Long date)
+  public void setDate(Date date)
   {
-    if(date instanceof Long)
-    {
-      this.date = (Long) date;
-    }
+    this.date = date;
   }
-  
+
   @Override
-  public Long getValue()
+  public Date getValue()
   {
     return this.date;
   }
-  
+
   @Override
   public void toJSON(JsonObject geoObjProps)
-  {  
+  {
     if (this.date != null)
     {
-      String sDate = this.date.toString();
-      
-      geoObjProps.addProperty(this.getName(), sDate);
+      geoObjProps.addProperty(this.getName(), this.date.getTime());
     }
   }
-  
+
   @Override
   public void fromJSON(JsonElement jValue, RegistryAdapter registry)
   {
-    this.setValue(jValue.getAsString());
+    long epoch = jValue.getAsLong();
+    this.setValue(new Date(epoch));
   }
 }
